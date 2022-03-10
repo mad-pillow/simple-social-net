12bd570c7027bc71b390c08d95452a0d304820a7
mad-pillow <dmtr.schv@gmail.com>
2022-03-10 22:08:17 +0200

    added feed page markup and styles

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main(props) {
   return (
     <div className='container flex-grow-1'>
-      <main className='d-flex flex-column justify-content-flex-start align-items-center'>{props.children}</main>
+      <main className='d-flex flex-column justify-content-start align-items-center'>{props.children}</main>
     </div>
   );
 }
