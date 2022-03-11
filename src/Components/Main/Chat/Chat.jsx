5d7aa26251055b67c2774c5ba4129b70bae053dd
mad-pillow <dmtr.schv@gmail.com>
2022-03-11 14:41:21 +0200

    added chat page markup and styles

--- src/Components/Main/Chat/Chat.jsx
+++ src/Components/Main/Chat/Chat.jsx
@@ -1,3 +1,39 @@
+import "./chat.scss";
+
 export default function Chat() {
-  return <h2>Chat</h2>;
+  return (
+    <>
+      <h2>Chat</h2>
+      <div className='chat-container container d-flex flex-grow-1 flex-column'>
+        <div className='messages-container flex-grow-1 d-flex flex-column'>
+          <div className='msg-block card d-inline-flex flex-column align-items-start align-self-end py-2 px-4 mb-1'>
+            <strong className='msg-owner mb-1'>@dmitriy.s</strong>
+            <p className='msg-text mb-1 small'>Hi, there!</p>
+            <small className='msg-time align-self-end text-muted'>15:02:45</small>
+          </div>
+          <div className='msg-block card d-inline-flex flex-column align-items-start align-self-end py-2 px-4 mb-1'>
+            <strong className='msg-owner mb-1'>@dmitriy.s</strong>
+            <p className='msg-text mb-1 small'>
+              Sunt sunt voluptas itaque! Nobis possimus. Libero asperiores? Id augue laboris. Dolorum, viverra velit,
+              torquent minim sapien quia debitis dolores netus mollit. Iaculis dui? Autem magni ultrices, doloribus
+              primis diamlorem corrupti class consequat semper! Diamlorem autem donec laudantium. Taciti quam, sequi
+              venenatis ducimus nunc cumque nisl! Corrupti interdum, laudantium lacinia.
+            </p>
+            <small className='msg-time align-self-end text-muted'>15:04:02</small>
+          </div>
+          <div className='msg-block card d-inline-flex flex-column align-items-start mw-80 align-self-start py-2 px-4'>
+            <strong className='msg-owner mb-1'>@pavel.k</strong>
+            <p className='msg-text mb-1 small'>Wow! That's awesome, man!</p>
+            <small className='msg-time align-self-end text-muted'>15:05:10</small>
+          </div>
+        </div>
+        <div className='input-group' bis_skin_checked='1'>
+          <input type='text' className='form-control' placeholder='Enter your message' />
+          <button type='submit' className='btn btn-secondary'>
+            Send
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main(props) {
   return (
-    <div className='container flex-grow-1'>
-      <main className='d-flex flex-column justify-content-start align-items-center'>{props.children}</main>
+    <div className='container flex-grow-1 d-flex'>
+      <main className='d-flex flex-grow-1 flex-column justify-content-start align-items-center'>{props.children}</main>
     </div>
   );
 }
